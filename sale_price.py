8cd5afa629c07c8f669a1d82813a21a621bcf100
Rharmon32 <88513773+Rharmon32@users.noreply.github.com>
2021-09-03 12:02:11 -0500

    Add files via upload

---
+++ sale_price.py
@@ -0,0 +1,12 @@
+#Get the original price of item
+original_price = float(input('Enter the original price of item: '))
+
+#Get the discount price of item
+discount = original_price * .2
+
+#Get the sale price of item
+sale_price = original_price - discount
+
+#Display the sale price.
+print(input('Is this item on sale? '))
+print('What is the cost of the item?', sale_price)
